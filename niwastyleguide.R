ba2ae55036230e1e0865cd9562df6b687e6cae4f
cmjt <cmjt@st-andrews.ac.uk>
2018-09-25 16:47:29 +1200

    removing footer etc

--- niwastyleguide.R
+++ niwastyleguide.R
@@ -28,11 +28,11 @@ niwaMenu <- function() {
         class = "collapse navbar-collapse",
         tags$ul(
           class = "nav navbar-nav",
-          # TODO: You can customize the menu links here
+          ## TODO: You can customize the menu links here
           tags$li(class = "active", tags$a(href = "/", "Home")),
-          tags$li(tags$a(href = "https://www.niwa.co.nz", "NIWA Web site")),
-          tags$li(
-            tags$a(href = "https://en.wikipedia.org/wiki/National_Institute_of_Water_and_Atmospheric_Research", "NIWA Wikipedia page")
+          tags$li(tags$a(href = "https://www.niwa.co.nz", "NIWA Web site",target="_blank")),
+          tags$li(tags$a(href = "https://en.wikipedia.org/wiki/National_Institute_of_Water_and_Atmospheric_Research",
+                          "NIWA Wikipedia page",target="_blank")
           )
         )
       )
@@ -40,38 +40,3 @@ niwaMenu <- function() {
   )
 }
 
-#' Add the NIWA banner, normally at the top.
-#'
-#' @param title The application title. Defaults to 'NIWA R Shiny Application'
-#' @param link The application link. Defaults to 'https://www.niwa.co.nz/'
-niwaBanner <- function(title = "NIWA R Shiny Application", link = "https://www.niwa.co.nz/") {
-  tags$div(class = "banner",
-           role = "banner",
-           tags$div(class = "container-fluid",
-                    tags$div(
-                      class = "row",
-                      # Set the title of your application here
-                      tags$a(title,
-                             class = "logo",
-                             href = link)
-                    )))
-}
-
-#' Add the applicatoin footer from NIWA.
-#'
-#' You can customize the footer links in this function.
-niwaFooter <- function() {
-  tags$footer(role = "contentinfo",
-              tags$div(class = "container",
-                       tags$div(
-                         class = "row",
-                         tags$a(href = "/", class = "logo"),
-                         tags$ul(
-                           # TODO: You can customize the header links here
-                           tags$li(
-                             tags$a(href = "https://www.niwa.co.nz/privacy-policy", "NIWA Privacy Policy")
-                           ),
-                           tags$li(id = "copyrightNiwa", class = "pull-right", "Copyright 2017 NIWA")
-                         )
-                       )))
-}
