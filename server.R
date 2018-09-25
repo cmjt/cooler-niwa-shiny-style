65da0acf79e0f2dbf24c40ce238e6f6105a5397b
cmjt <cmjt@st-andrews.ac.uk>
2018-09-25 16:48:57 +1200

    simplify style

--- server.R
+++ server.R
@@ -1,26 +1,19 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+##
+## Find out more about building applications with Shiny here:
+## 
+##    http://shiny.rstudio.com/
 
 library(shiny)
 
-# Define server logic required to draw a histogram
+## Define server logic required to draw a histogram
+## simple example of a histogram from the old faithful dataset
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
+    output$distPlot <- renderPlot({
+        ## generate bins based on the input$bins from ui.R
+        x    <- faithful[, 2] 
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        ## draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
-  
 })
--- ui.R
+++ ui.R
@@ -1,51 +1,41 @@
-# Application: R Shiny example with NIWA Style guide (https://styles.niwa.co.nz).
-# Author: NIWA/ITSD
-# Source: https://git.niwa.local/ITSD/shiny-styleguide
-# Issues: https://git.niwa.local/ITSD/shiny-styleguide/issues
-# For more information: https://styles.niwa.co.nz
+## Application: R Shiny example with
+## Original author NIWA/ITSD cmjt revised as currently outdated logo
 
 library(shiny)
 
-# Load UI customization functions
+## Load UI customization functions
 source('./niwastyleguide.R')
 
-# Define UI for application that draws a histogram
+## Define UI for application that draws a histogram
 shinyUI(
-  fluidPage(
-    theme = "https://styles.niwa.co.nz/dist/css/bootstrap.min.css",
-    style = "margin: 0px !important; border: 0px !important; padding: 0px !important;",
-    # NIWA Style guide. See https://styles.niwa.co.nz for more.
-    tags$head(
-      tags$link(rel = "stylesheet", type = "text/css", href = "https://styles.niwa.co.nz/dist/css/bootstrap-theme.min.css")
-    ),
-    
-    # NIWA Banner
-    niwaBanner(title = 'NIWA R Shiny App', link = 'https://github.com/niwa/'),
-    
-    niwaMenu(),
-    tags$br(class = "clearfix;"),
-    
-    # two columns layout
-    tags$div(class = "row-fluid",
-             sidebarPanel(
-               # TODO: your sidebar content goes here
-               sliderInput(
-                 "bins",
-                 "Number of bins:",
-                 min = 1,
-                 max = 50,
-                 value = 30
-               )
+    fluidPage(
+        includeCSS("style.css"), ## call .css file in the same wd
+        title = "NIWA Shiny App",
+        style = "margin: 0px !important; border: 0px !important; padding: 0px !important;",
+        tags$head(
+                 tags$div(class="head-im",
+                          img(src="https://www.niwa.co.nz/sites/niwa.co.nz/files/niwa-2018-horizontal-final-400_0.png",
+                              alt="NIWA horizontal logo",id= "logo"))
              ),
-             mainPanel(# TODO: your main content goes here
-               plotOutput("distPlot"))),
-    
-    niwaFooter(),
-    
-    # Placed at the end of the document so the pages load faster
-    tags$head(
-      tags$script(src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"),
-      tags$script(src = "https://styles.niwa.co.nz/dist/js/bootstrap.min.js")
+        tags$br(), ## break
+        ## two columns layout
+        tags$div(class = "row-fluid",
+                 sidebarPanel(
+                     sliderInput(
+                         "bins",
+                         "Number of bins:",
+                         min = 1,
+                         max = 50,
+                         value = 30
+                     )
+                 ),
+                 mainPanel(## Main content goes here (simple histogram at the moment)
+                     plotOutput("distPlot"))
+                 ), ## end of two-column layout
+        tags$br(), ## break
+        ## footer
+        tags$div(tags$footer(class="foot",
+                             tags$img(src="NIWA_Rev_Hor_2.png", alt="reverse NIWA logo",height="30",width="70"))
+                 )
     )
-  )
-) # end
+) ## end of shiny UI
